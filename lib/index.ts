824acb075a8986c06cfac3d33aa35894e85bb4c7
Martin Blom <martin@blom.org>
2017-03-04 18:20:22 +0100

    Added a CSV/TSV parser.

--- lib/index.ts
+++ lib/index.ts
@@ -4,20 +4,26 @@ export * from './uri';
 import { Parser } from './parsers';
 import { URI }    from './uri';
 
-import { FileProtocol } from './protocols/file';
-import { HTTPProtocol } from './protocols/http';
-
 // asyncIterator polyfill
 (Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
 
 // Register all built-in protocols
+import { FileProtocol } from './protocols/file';
+import { HTTPProtocol } from './protocols/http';
+
 URI
     .register('file',  FileProtocol)
     .register('http',  HTTPProtocol)
     .register('https', HTTPProtocol)
 ;
 
+// Register all built-in parsers
+import { CSVParser }  from './parsers/csv';
 import { HTMLParser } from './parsers/html';
 
 Parser
-    .register('text/html', HTMLParser);
+    .register('text/csv',                   CSVParser)
+    .register('text/html',                  HTMLParser)
+    .register('text/tab-separated-values',  CSVParser)
+    .register('text/tsv' /* Unofficial */,  CSVParser)
+;
